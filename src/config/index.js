d8ea833d5b734e0e75c62e0b1fd5e9bc8e133f58
Anas Qadil <qadilanass10@gmail.com>
2022-10-24 20:08:01 +0100

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const links = {
   // This is the link to the API server
-  localhost: 'http://localhost:3001',
+  localhost: 'https://poised-lion-capris.cyclic.app/',
   // This is the link to the hosted server
   server: ""
 }
